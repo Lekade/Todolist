fa23e240737147c6fbd2df3b3128db15db9b5547
Денис Касперович <kasperovich.deonis@gmail.com>
2024-06-26 22:51:51 +0300

    changing todolist with added logic in the app and allocating some code to components

--- src/components/todolist/Todolist.tsx
+++ src/components/todolist/Todolist.tsx
@@ -1,6 +1,9 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import React from 'react';
 import {FilterValuesType, TaskType} from "../../App";
 import {Button} from "../button/Button";
+import AddItemForm from "../addItemForm/addItemForm";
+import {TransformTitle} from "../transformTItle/TransformTitle";
+import Task from "../task/Task";
 
 type PropsType = {
     todoListId: string
@@ -11,58 +14,74 @@ type PropsType = {
     addTask: (taskTitle: string, todoListId: string) => void
     changeStatusTask: (taskId: string, isDone: boolean, todoListId: string) => void
     filter: FilterValuesType
+    removeToDoList: (todoListId: string) => void
+    changeToDoListTitle: (newToDoListTitle: string, todoListId: string) => void
+    changeTaskTitle: (newTaskTitle: string, taskId: string, todoListId: string) => void
 }
 
-export const Todolist = ({todoListId, title, tasks, removeTask,  changeTaskFilter, addTask, changeStatusTask, filter}: PropsType) => {
+export const Todolist = ({
+                             todoListId,
+                             title,
+                             tasks,
+                             removeTask,
+                             changeTaskFilter,
+                             addTask,
+                             changeStatusTask,
+                             filter,
+                             removeToDoList,
+                             changeToDoListTitle,
+                             changeTaskTitle
+                         }: PropsType) => {
+    const removeToDoListHandler = () => {
+        removeToDoList(todoListId)
+    }
+    const addTaskHandler = (taskTitle: string) => {
+        addTask(taskTitle, todoListId)
+    }
+    const changeToDoListTitleHandler = (newToDoListTitle: string) => {
+        changeToDoListTitle(newToDoListTitle, todoListId)
+    }
+    const changeTaskTitleHandler = (newTaskTitle: string, taskId: string) => {
+        changeTaskTitle(newTaskTitle, taskId, todoListId)
+    }
+    const changeStatusTaskHandler = (taskId: string, isDone: boolean) => {
+        changeStatusTask(taskId, isDone, todoListId)
+    }
 
-    const [inputText, setInputText] = useState('')
-    const[error, setError] = useState(false)
-    const ChangeInputHandler = (e:ChangeEvent<HTMLInputElement>) =>{
-        setInputText(e.currentTarget.value)
-        setError(false)
-    }
-    const addTaskInput = () => {
-        if(inputText.trim()){
-            setError(false)
-            addTask(inputText.trim(), todoListId)
-            setInputText('')
-        }else {
-            setError(true)
-        }
-    }
-
-    const keyDownAddTaskInput = (e:KeyboardEvent<HTMLInputElement>) =>{
-        e.key === 'Enter' && addTaskInput()
-    }
 
     return (
         <div className='todolist'>
-            <h3>{title}</h3>
-            <div>
-                <input className={error ? 'error-input' : ''} onChange={ChangeInputHandler} onKeyDown={keyDownAddTaskInput} value={inputText} />
-                <Button onClickHandler={addTaskInput} title='+'/>
-            </div>
-            {error && <div className='error'>The field is required</div>}
+            <TransformTitle changeTitle={newTitle => changeToDoListTitleHandler(newTitle)} title={title}/>
+            <Button onClickHandler={removeToDoListHandler} title='X'/>
+            <AddItemForm addItem={(itemTitle) => addTaskHandler(itemTitle)}/>
             {
                 tasks.length === 0
                     ? <p>Your taskslist is empty</p> :
                     <ul>
                         {tasks.map(task => {
-                            return (
-                                <li key={task.taskId}>
-                                    <input type="checkbox" onChange={(e) => changeStatusTask(task.taskId, e.currentTarget.checked, todoListId)} checked={task.isDone}/>
-                                    <span className={task.isDone ? 'task-done' : 'task'} >{task.title}</span>
-                                    <button onClick={() => removeTask(task.taskId, todoListId)}>X</button>
-                                </li>
-                            )
+                            return <Task
+                                key={task.taskId}
+                                taskId={task.taskId}
+                                isDone={task.isDone}
+                                title={task.title}
+                                changeStatusTask={isDone => changeStatusTaskHandler(task.taskId, isDone)}
+                                changeTaskTitleHandler={newTaskTitle => changeTaskTitleHandler(newTaskTitle, task.taskId)}
+                                removeTask={() => removeTask(task.taskId, todoListId)}
+                            />
                         })}
                     </ul>
             }
             <div>
-                <Button classes={filter === 'all' ? 'activeFilter' : ''} onClickHandler={() => changeTaskFilter('all', todoListId)} title='All'/>
-                <Button classes={filter === 'active' ? 'activeFilter' : ''} onClickHandler={() => changeTaskFilter('active', todoListId)} title='Active'/>
-                <Button classes={filter === 'completed' ? 'activeFilter' : ''} onClickHandler={() => changeTaskFilter('completed', todoListId)} title='Completed'/>
+                <Button classes={filter === 'all' ? 'activeFilter' : ''}
+                        onClickHandler={() => changeTaskFilter('all', todoListId)} title='All'/>
+                <Button classes={filter === 'active' ? 'activeFilter' : ''}
+                        onClickHandler={() => changeTaskFilter('active', todoListId)} title='Active'/>
+                <Button classes={filter === 'completed' ? 'activeFilter' : ''}
+                        onClickHandler={() => changeTaskFilter('completed', todoListId)} title='Completed'/>
             </div>
         </div>
     )
 }
+
+
+
