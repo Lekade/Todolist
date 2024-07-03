f68e4de7e9429e804e4ad3fd3a7e792b318e55c9
Денис Касперович <kasperovich.deonis@gmail.com>
2024-07-03 14:42:51 +0300

    stylized with Mui: checkbox, title, delete button in the form of an icon

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import {TransformTitle} from "../transformTItle/TransformTitle";
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ListItem from '@mui/material/ListItem';
 
 type TaskType = {
     taskId: string
@@ -10,16 +14,19 @@ type TaskType = {
     removeTask: (taskId: string) => void
 }
 
-const Task = ({taskId, isDone, title, changeStatusTask, changeTaskTitleHandler, removeTask}:TaskType) => {
+export const Task = ({taskId, isDone, title, changeStatusTask, changeTaskTitleHandler, removeTask}:TaskType) => {
     return (
-        <li>
-            <input type="checkbox"
-                   onChange={e => changeStatusTask(e.currentTarget.checked)}
-                   checked={isDone}/>
+        <ListItem sx={{
+            p: 0,
+            justifyContent: 'space-between',
+            opacity: isDone ? 0.5 : 1,
+        }}>
+            <Checkbox onChange={e => changeStatusTask(e.currentTarget.checked)} checked={isDone}/>
             <TransformTitle style={isDone ? 'task-done' : 'task'}  changeTitle={newTitle =>  changeTaskTitleHandler(newTitle)} title={title}/>
-            <button onClick={() => removeTask(taskId)}>X</button>
-        </li>
+            <IconButton aria-label="delete" onClick={() => removeTask(taskId)}>
+                <DeleteIcon fontSize="inherit"/>
+            </IconButton>
+        </ListItem>
     );
 };
 
-export default Task;
