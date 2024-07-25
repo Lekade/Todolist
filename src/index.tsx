abcbe9e9534d09af17d5f51ec12fc0506f2d9a0c
Денис Касперович <kasperovich.deonis@gmail.com>
2024-07-25 13:43:42 +0300

    store added in context all app

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {Provider} from "react-redux";
+import {store} from "./reducers/Store";
+import App from "./App";
 
 
 const root = ReactDOM.createRoot(
@@ -9,7 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App/>
+      </Provider>
   </React.StrictMode>
 );
 
