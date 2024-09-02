6ffe1d651c59028e43808c40ed6b32e32f50bd00
Денис Касперович <kasperovich.deonis@gmail.com>
2024-09-02 10:28:50 +0300

    created hook useAppDispatch (can now accept thunk)

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,6 +1,8 @@
-import { tasksReducer } from './tasks-reducer'
-import { todolistsReducer } from './todolists-reducer'
-import {combineReducers, legacy_createStore} from 'redux'
+import { tasksReducer } from './tasks-reducer';
+import { todolistsReducer } from './todolists-reducer';
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from 'redux';
+import {thunk, ThunkDispatch} from "redux-thunk";
+import {useDispatch} from "react-redux";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -9,9 +11,12 @@ export const rootReducer = combineReducers({
     todolists: todolistsReducer
 })
 // непосредственно создаём store
-export const store = legacy_createStore(rootReducer)
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
+export  type AppDispatchType = ThunkDispatch<AppRootStateType, unknown, AnyAction>
+
+export const useAppDispatch = useDispatch<AppDispatchType>
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
-window.store = store
+window.store = store;
