fb4e7011f8558d68754ccba7d057a452f56547dc
Денис Касперович <kasperovich.deonis@gmail.com>
2024-09-19 18:19:39 +0300

    added Login component (redirect to Todolists) (used formik) and added ErrorPage component

---
+++ src/components/errorPage/ErrorPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const ErrorPage = () => {
+    return (
+        <div className="error-page">
+            <div>
+                <h1>Oops!</h1>
+                <p>Page not found 404</p>
+            </div>
+        </div>
+    );
+}
