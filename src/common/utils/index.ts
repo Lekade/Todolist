9ef799cf960e08fb77761fc23cad2f0e4a554438
Денис Касперович <kasperovich.deonis@gmail.com>
2024-10-14 13:38:35 +0300

    remote utility functions for error handling and application status changes

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,3 +1,1 @@
-export { handleServerAppError } from "./handle-server-app-error"
-export { handleServerNetworkError } from "./handle-server-network-error"
 export { createAppAsyncThunk } from "./create-app-async-thunk"
